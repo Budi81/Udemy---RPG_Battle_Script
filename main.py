a0ec781cbb968002da22bf4dbfc61d70f91329ea
Lukasz Budzyn <likasz.budzyn81@gmail.com>
2020-03-27 20:30:55 +0100

    Added Class Magic

--- main.py
+++ main.py
@@ -1,11 +1,74 @@
 from classes.game import Person, Bcolors
+from classes.magic import Spell
 
-magic = [{"name": "Fire", "cost": 10, "dmg": 60},
-         {"name": "Thunder", "cost": 10, "dmg": 60},
-         {"name": "Blizzard", "cost": 10, "dmg": 60}]
+# Create Black Magic
+fire = Spell("Fire", 10, 100, "black")
+thunder = Spell("Thunder", 10, 100, "black")
+blizzard = Spell("Blizzard", 10, 100, "black")
+meteor = Spell("Meteor", 20, 200, "black") 
+quake = Spell("Quake", 14, 140, "black")
 
+# Create White Magic
+cure = Spell("Cure", 12, 120, "white")
+cura = Spell("Cura", 18, 200, "white")
 
-player = Person(460, 65, 60, 34, magic)
+# Instantiate People
+player = Person(460, 65, 60, 34, [fire, thunder, blizzard, meteor, cure, cura])
+enemy = Person(1200, 65, 45, 25, [])
 
-print(player.generate_spell_damage(0))
-print(player.generate_spell_damage(1))
+running = True
+
+print(Bcolors.FAIL + Bcolors.BOLD + "An ENEMY ATTACKS!" + Bcolors.ENDC)
+
+while running:
+    print("========================")
+    player.choose_action()
+    choice = input("Choose action: ")
+    index = int(choice) - 1
+
+    if index == 0:
+        dmg = player.generate_damage()
+        enemy.take_damage(dmg)
+        print("You have attacked for", dmg, "points od damage.") 
+    
+    elif index == 1:    
+        player.choose_magic()
+        magic_choice = int(input("Choose spell:")) - 1
+
+        spell = player.magic[magic_choice]
+        magic_dmg = spell.generate_damage()
+
+        current_mp = player.get_mp()
+
+        if spell.cost > current_mp:
+            print(Bcolors.FAIL + "\nNot enough MP\n" + Bcolors.ENDC)
+            continue
+
+        player.reduce_mp(spell.cost)
+
+        if spell.type == "white":
+            player.heal(magic_dmg)
+            print(Bcolors.OKBLUE + "\n" + spell.name + "heals for", str(magic_dmg), "HP." + Bcolors.ENDC)
+        elif spell.type == "black":
+            enemy.take_damage(magic_dmg)
+            print(Bcolors.OKBLUE + "\n" + spell.name + " deals", str(magic_dmg), "points of damage." + Bcolors.ENDC)
+
+    enemy_choice = 1
+
+    enemy_damage = enemy.generate_damage()
+    player.take_damage(enemy_damage)
+    print("Enemy attacks for:", enemy_damage, "points od damage.")
+
+    print("=========================")
+    print("Enemy HP:", Bcolors.FAIL + str(enemy.get_hp()) + "/" + str(enemy.get_max_hp()) + Bcolors.ENDC +"\n")
+
+    print("Your HP:" + Bcolors.OKGREEN + str(player.get_hp()) + "/" + str(player.get_max_hp()) + Bcolors.ENDC)
+    print("Your MP:" + Bcolors.OKBLUE + str(player.get_mp()) + "/" + str(player.get_max_mp()) + Bcolors.ENDC + "\n")
+
+    if enemy.get_hp() == 0:
+        print(Bcolors.OKGREEN + "You win!" + Bcolors.ENDC)
+        running = False
+
+    elif player.get_hp() == 0:
+        print(Bcolors.FAIL + "Your enemy has defeated you!" + Bcolors.ENDC)
+        running = False
