2fcaf4a87aa71b449b584a237fc29efa775b55b2
Lukasz Budzyn <likasz.budzyn81@gmail.com>
2020-03-28 18:12:50 +0100

    Added working HP and MP bars

--- classes/game.py
+++ classes/game.py
@@ -94,6 +94,55 @@ class Person:
 
 
     def get_stats(self):
-        print("                       _________________________         __________")
-        print(Bcolors.BOLD + self.name + "      " + str(self.hp) + "/" + str(self.maxhp) + " |" + Bcolors.FAIL +"█████████████████████████" + Bcolors.ENDC + Bcolors.BOLD +
-              " |" + str(self.mp) + "/" + str(self.maxmp) + " |" + Bcolors.OKBLUE + "██████████" + Bcolors.ENDC + "|")
+        hp_bar = ""
+        bar_ticks = (self.hp / self.maxhp) * 100 / 4
+
+        mp_bar = ""
+        mp_ticks = (self.mp / self.maxmp) * 100 / 10
+
+        while bar_ticks > 0:
+            hp_bar += "█"
+            bar_ticks -= 1
+
+        while len(hp_bar) < 25:
+            hp_bar += " "
+
+        while mp_ticks > 0:
+            mp_bar += "█"
+            mp_ticks -= 1
+
+        while len(mp_bar) < 10:
+            mp_bar += " "
+
+        hp_string = str(self.hp) + "/" + str(self.maxhp)
+        current_hp = ""
+
+        if len(hp_string) < 9:
+            decreased = 9 - len(hp_string)
+
+            while decreased > 0:
+                current_hp += " "
+                decreased -= 1
+
+            current_hp += hp_string
+        else:
+            current_hp = hp_string
+
+        mp_string = str(self.mp) + "/" + str(self.maxmp)
+        current_mp = ""
+
+        if len(mp_string) < 7:
+            decreased = 7 - len(mp_string)
+
+            while decreased > 0:
+                current_mp += " "
+                decreased -= 1
+
+            current_mp += mp_string
+        else:
+            current_mp = mp_string
+
+        print("                       _________________________          __________")
+        print(Bcolors.BOLD + self.name + "      " + current_hp + " |" + Bcolors.FAIL + 
+              hp_bar + Bcolors.ENDC + Bcolors.BOLD + "|" + current_mp + " |" + 
+              Bcolors.OKBLUE + mp_bar + Bcolors.ENDC + "|")
--- main.py
+++ main.py
@@ -4,21 +4,21 @@ from classes.inventory import Item
 
 
 # Create Black Magic
-fire = Spell("Fire", 10, 100, "black")
-thunder = Spell("Thunder", 10, 100, "black")
-blizzard = Spell("Blizzard", 10, 100, "black")
-meteor = Spell("Meteor", 20, 200, "black") 
+fire = Spell("Fire", 25, 600, "black")
+thunder = Spell("Thunder", 25, 600, "black")
+blizzard = Spell("Blizzard", 25, 600, "black")
+meteor = Spell("Meteor", 40, 1200, "black") 
 quake = Spell("Quake", 14, 140, "black")
 
 # Create White Magic
-cure = Spell("Cure", 12, 120, "white")
-cura = Spell("Cura", 18, 200, "white")
+cure = Spell("Cure", 25, 620, "white")
+cura = Spell("Cura", 32, 1500, "white")
 
 
 # Create some Items
 potion = Item("Potion", "potion", "Heals 50 HP", 50)
 hipotion = Item("Hi-Potion", "potion", "Heals 100 HP", 100)
-superpotion = Item("Super Potion", "potion", "Heals 500 HP", 500)
+superpotion = Item("Super Potion", "potion", "Heals 500 HP", 1000)
 elixier = Item("Elixier", "elixier", "Fully restores HP/MP of one party member", 9999)
 hielixer = Item("MegaElixer", "elixer", "Fully restores party's HP/MP", 9999)
 
@@ -32,10 +32,10 @@ player_items = [{"item": potion, "quantity": 15}, {"item": hipotion, "quantity":
 
 
 # Instantiate People
-player1 = Person("Valos:", 3260, 65, 60, 34, player_spells, player_items)
-player2 = Person("Nick :", 4160, 65, 60, 34, player_spells, player_items)
-player3 = Person("Robot:", 3089, 65, 60, 34, player_spells, player_items)
-enemy = Person("Magus", 1200, 65, 45, 25, [], [])
+player1 = Person("Valos:", 3260, 132, 300, 34, player_spells, player_items)
+player2 = Person("Nick :", 4160, 188, 311, 34, player_spells, player_items)
+player3 = Person("Robot:", 3089, 174, 288, 34, player_spells, player_items)
+enemy = Person("Magus", 11200, 221, 525, 25, [], [])
 
 players = [player1, player2, player3]
 
@@ -45,13 +45,11 @@ print(Bcolors.FAIL + Bcolors.BOLD + "An ENEMY ATTACKS!" + Bcolors.ENDC)
 
 while running:
     print("========================")
-
-    print("\n\n")
-    print("NAME                   HP                                MP")
+    print("\n")
+    print("NAME                   HP                                 MP")
     for player in players:
         player.get_stats()
 
-    print("\n")
 
     for player in players:
         player.choose_action()
@@ -61,9 +59,9 @@ while running:
         if index == 0:
             dmg = player.generate_damage()
             enemy.take_damage(dmg)
-            print("You have attacked for", dmg, "points od damage.") 
-        
-        elif index == 1:    
+            print("You have attacked for", dmg, "points od damage.")
+
+        elif index == 1:
             player.choose_magic()
             magic_choice = int(input("    Choose spell: ")) - 1
             
